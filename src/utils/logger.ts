843c0bda493468cfe832f553f6308191bc3bfc8f
FLzyy <flzyy72@gmail.com>
2023-02-07 12:38:13 +0800

    fixes + allow-all

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -9,33 +9,41 @@ const levels = {
   FATAL: kulay.bgRed,
 };
 
-interface Log {
-  trace(text: unknown): this;
-  debug(text: unknown): this;
-  info(text: unknown): this;
-  warn(text: unknown): this;
-  error(text: unknown): this;
-  fatal(text: unknown): this;
-}
+const f = (level: keyof typeof levels, text: unknown) => {
+  (console[level as keyof Console] as typeof console.log)(
+    `[${
+      (new Intl.DateTimeFormat("en", {
+        timeStyle: "full",
+      })).format(Date.now())
+    }] ${levels[level](level)} (${Deno.pid}): ${kulay.blue(String(text))}`,
+  );
+};
 
-const log = {} as Log;
-
-for (const key of Object.keys(levels)) {
-  Object.defineProperty(log, key, {
-    [key.toLowerCase()](text: unknown) {
-      (console[key as keyof Console] as typeof console.log)(
-        `[${
-          (new Intl.DateTimeFormat("en", {
-            timeStyle: "full",
-          })).format(Date.now())
-        }] ${levels[key as keyof typeof levels](key)} (${Deno.pid}): ${
-          kulay.blue(String(text))
-        }`,
-      );
-
-      return this;
-    },
-  });
-}
+const log = {
+  trace(text: unknown) {
+    f("TRACE", text);
+    return this;
+  },
+  debug(text: unknown) {
+    f("DEBUG", text);
+    return this;
+  },
+  info(text: unknown) {
+    f("INFO", text);
+    return this;
+  },
+  warn(text: unknown) {
+    f("WARN", text);
+    return this;
+  },
+  error(text: unknown) {
+    f("ERROR", text);
+    return this;
+  },
+  fatal(text: unknown) {
+    f("FATAL", text);
+    return this;
+  },
+};
 
 export default log;
